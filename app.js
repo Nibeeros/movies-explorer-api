b7dcc446bcd9dd277d2f48d999a4e2e78eafc04f
Nibeeros <nibeeros@gmail.com>
2023-09-23 19:33:09 +0300

    достал корс

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { corsHandler } = require('./middlewares/cors');
+// const { corsHandler } = require('./middlewares/cors');
 const { limiter } = require('./middlewares/limiter');
 
 const errorHandler = require('./middlewares/errorHandler');
 const { MONGOOSE_DB } = require('./constants/constants');
 
-const { PORT = 3001 } = process.env;
+const origin = [
+  'http://localhost:3001',
+  'http://localhost:3000',
+  'http://localhost:4000',
+  'http://localhost:4001',
+  'http://nibfilm.nomoredomainsicu.ru',
+  'https://nibfilm.nomoredomainsicu.ru',
+];
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
 mongoose.connect(MONGOOSE_DB);
@@ -20,7 +30,8 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
-app.use(corsHandler);
+// app.use(corsHandler);
+app.use(cors({ origin, credentials: true, maxAge: 40 }));
 
 app.use('/', limiter, require('./routes/index'));
 
@@ -29,4 +40,4 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+app.listen(PORT);
